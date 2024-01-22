654d43875ee22f7aaae18b26978ac9e2ff22eb51
thatplatypus <tombrewer0@gmail.com>
2024-01-21 22:24:43 -0700

    Extra Dependency Injection wiring options

--- Flagzor/Extensions/ServiceCollectionExtensions.cs
+++ Flagzor/Extensions/ServiceCollectionExtensions.cs
@@ -21,16 +21,73 @@ namespace Flagzor.Extensions
             return services;
         }
 
+        public static IServiceCollection AddFeatureFlagzor<T>(this IServiceCollection services, Action<FeatureFlagConfiguration> configure)
+            where T : class, IFeatureFlagService
+        {
+            var configuration = new FeatureFlagConfiguration();
+            configure(configuration);
+
+            services.AddSingleton(configuration);
+            services.AddScoped<IFeatureFlagService, T>();
+            services.AddScoped<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFeatureFlagzor<TService, TProvider>(this IServiceCollection services, Action<FeatureFlagConfiguration> configure)
+            where TService : class, IFeatureFlagService
+            where TProvider : FeatureFlagStateProvider
+        {
+            var configuration = new FeatureFlagConfiguration();
+            configure(configuration);
+
+            services.AddSingleton(configuration);
+            services.AddScoped<IFeatureFlagService, TService>();
+            services.AddScoped<FeatureFlagStateProvider, TProvider>();
+
+            return services;
+        }
+
         public static IServiceCollection AddFeatureFlagzor(this IServiceCollection services, IConfiguration configuration)
         {
-            var featureFlagConfiguration = configuration.GetSection(_configSectionName).Get<FeatureFlagConfiguration>() 
-                ?? throw new InvalidOperationException($"Missing '{_configSectionName}' section in configuration.");
-            
+            var featureFlagConfiguration = GetFeatureFlagConfiguration(configuration);
+
             services.AddSingleton(featureFlagConfiguration);
             services.AddScoped<IFeatureFlagService, FeatureFlagService>();
             services.AddScoped<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
 
             return services;
+        }
+
+        public static IServiceCollection AddFeatureFlagzor<T>(this IServiceCollection services, IConfiguration configuration)
+            where T : class, IFeatureFlagService
+        {
+            var featureFlagConfiguration = GetFeatureFlagConfiguration(configuration);
+
+            services.AddSingleton(featureFlagConfiguration);
+            services.AddScoped<IFeatureFlagService, T>();
+            services.AddScoped<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFeatureFlagzor<TService, TProvider>(this IServiceCollection services, IConfiguration configuration)
+            where TService : class, IFeatureFlagService
+            where TProvider : FeatureFlagStateProvider
+        {
+            var featureFlagConfiguration = GetFeatureFlagConfiguration(configuration);
+
+            services.AddSingleton(featureFlagConfiguration);
+            services.AddScoped<IFeatureFlagService, TService>();
+            services.AddScoped<FeatureFlagStateProvider, TProvider>();
+
+            return services;
+        }
+
+        private static FeatureFlagConfiguration GetFeatureFlagConfiguration(IConfiguration configuration)
+        {
+            return configuration.GetSection(_configSectionName).Get<FeatureFlagConfiguration>()
+                ?? throw new InvalidOperationException($"Missing '{_configSectionName}' section in configuration.");
         }
     }
 }
