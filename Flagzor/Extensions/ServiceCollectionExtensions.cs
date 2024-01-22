1b8dd203b0a2d6a83649f0b34c569fe44ea9053f
thatplatypus <tombrewer0@gmail.com>
2024-01-21 21:55:35 -0700

    Service Collection Options

--- Flagzor/Extensions/ServiceCollectionExtensions.cs
+++ Flagzor/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,34 @@
 ﻿using Flagzor.Components;
 using Flagzor.Configuration;
 using Flagzor.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Flagzor.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        private const string _configSectionName = "Flagzor";
         public static IServiceCollection AddFeatureFlagzor(this IServiceCollection services, Action<FeatureFlagConfiguration> configure)
         {
             var configuration = new FeatureFlagConfiguration();
             configure(configuration);
 
             services.AddSingleton(configuration);
+            services.AddScoped<IFeatureFlagService, FeatureFlagService>();
+            services.AddScoped<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
 
-            services.AddSingleton<IFeatureFlagService, FeatureFlagService>();
-            services.AddSingleton<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
+            return services;
+        }
+
+        public static IServiceCollection AddFeatureFlagzor(this IServiceCollection services, IConfiguration configuration)
+        {
+            var featureFlagConfiguration = configuration.GetSection(_configSectionName).Get<FeatureFlagConfiguration>() 
+                ?? throw new InvalidOperationException($"Missing '{_configSectionName}' section in configuration.");
+            
+            services.AddSingleton(featureFlagConfiguration);
+            services.AddScoped<IFeatureFlagService, FeatureFlagService>();
+            services.AddScoped<FeatureFlagStateProvider, DefaultFeatureFlagStateProvider>();
 
             return services;
         }
