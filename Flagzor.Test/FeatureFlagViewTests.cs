1eba543dd45246e578049a0005ab69a4780e79c3
thatplatypus <tombrewer0@gmail.com>
2024-01-23 22:26:51 -0700

    Add FeatureFlagViewTests

---
+++ Flagzor.Test/FeatureFlagViewTests.cs
@@ -0,0 +1,75 @@
+﻿using Flagzor.Configuration;
+using Flagzor.Extensions;
+
+namespace Flagzor.Test
+{
+    public class FeatureFlagViewTests : TestContext
+    {
+        public FeatureFlagViewTests()
+        {
+            Services.AddFeatureFlagzor(config =>
+            {
+                config.FeatureFlags.Add(new FeatureFlag
+                {
+                    Feature = "Beta",
+                    Enabled = true
+                });
+            });
+        }
+
+        [Fact]
+        public void FeatureFlagView_ShouldRenderChildContent_WhenFeatureEnabled()
+        {
+            // Act
+            var cut = RenderComponent<SimpleBetaFeature>();
+
+            // Assert
+            cut.MarkupMatches("Enabled Beta Feature");
+        }
+
+        [Fact]
+        public void FeatureFlagView_ShouldRenderEmptyyChildContent_WhenFeatureNotEnabled()
+        {
+            // Arrange
+            var config = Services.GetService<FeatureFlagConfiguration>();
+            var feature = config.FeatureFlags.FirstOrDefault(x => x.Feature == "Beta");
+            feature.Enabled = false;
+
+            // Act
+            var cut = RenderComponent<SimpleBetaFeature>();
+
+            // Assert
+            cut.MarkupMatches("");
+        }
+
+        [Fact]
+        public void FeatureFlagView_ShouldRenderActiveContent_WhenFeatureEnabled()
+        {
+            //Arrange
+            Services.AddFeatureFlagzor(config =>
+            {
+                config.FeatureFlags.Add(new FeatureFlag
+                {
+                    Feature = "Test",
+                    Enabled = true
+                });
+            });
+
+            // Act
+            var cut = RenderComponent<TestFeatureComponent>();
+
+            // Assert
+            cut.MarkupMatches("This is shown when the feature flag is enabled.");
+        }
+
+        [Fact]
+        public void FeatureFlagView_ShouldRenderInactiveContent_WhenFeatureNotEnabled()
+        {
+            // Act
+            var cut = RenderComponent<TestFeatureComponent>();
+
+            // Assert
+            cut.MarkupMatches("This is shown when the feature flag is disabled.");
+        }
+    }
+}
---
+++ Flagzor.Test/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Xunit;
+global using Bunit;
+global using Shouldly;
+global using NSubstitute;
+global using System;
+global using Flagzor;
+global using Flagzor.Test.Components;
